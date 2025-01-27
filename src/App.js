0cfd3e86ccc1afa10bbb54be3f6cb22b6d8df565
MrRiceGuy <carsonkeene@gmail.com>
2025-01-27 10:57:34 -0500

    Initial Commit for Center of Socail Creativity

---
+++ src/App.js
@@ -0,0 +1,178 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" title = "CSC">
+        <h1>Center for Social Creativity </h1>
+        <p alt = "CSU"></p>
+
+
+      </header>
+
+
+      <div className = "main">
+        <div> 
+          
+          <div className='top'>
+            <div className='logo'></div>
+            <div className='nav'>
+              <a href =''>Home</a>
+              <a href =''>Services</a>
+              <a href =''>Team</a>
+              <a href =''>Contact</a>
+
+            </div>
+          </div>
+
+          <div className='section-1'>
+            <h1>Building Resilience for Health Equity</h1>
+
+            <div className='meetwithus'><a href =''>Meet with Us</a></div>
+            <div className='meetwithus'><a href =''>Meet Our Team</a></div>
+          </div>
+
+          <div className='section-2'>
+            <ul>
+              <li><h2>Assessment</h2>
+              <p>Know where you're going and why, with full community buy-in, and continually improve.</p></li>
+              
+              <li><h2>People</h2><p>Build COhesive and energized teams that understand and tust each other.</p></li>
+              
+              <li><h2>Processes</h2><p>Clearly map your mission's success, and activate support to overcome barriers.</p></li>
+              
+              <li><h2>Resources</h2><p>Fully fund and equip your initiatives for success and impact</p></li>
+            </ul>
+          </div>
+
+          <div className='section-3'>
+            <h2>Solving big roblems takes time.</h2>
+            <h2 className='yellow'>Are you ready for that journey?</h2>
+            <ul>
+              <li><h3>Are you uncertain about the root causes of your community's challenges?</h3></li>
+              <li><h3>Is it hard to see how all the pieces fit together?</h3></li>
+              <li><h3>Are the next steps unclear or confusing?</h3></li>
+              <li><h3>Is your team overwhelmed, turning over, or struggling to hire?</h3></li>
+              <li><h3>Are you chronically under-resourced</h3></li>
+              <li><h3>Do you wonder how you can possibly do everything that is needed?</h3></li>
+            </ul>
+
+          </div>
+
+          <div className='section-4'>
+              <h1>
+              You have the passion for the mission.</h1>
+
+              <h2>We ensure you have the understanding, team, methods, and tools to succeed.</h2>
+          </div>    
+
+          <div className='section-5'>
+            <div className='mountainousbackground left'>
+              <h1>Built To Last</h1>
+
+              <h3>We help changemakers and mission-driven organizations build resiliency at the organizational level to promote health equity within their own communities. You are creating health equity; we support you in establishing the practices and systems to keep your mission thriving and sustainable.</h3>
+            </div>
+
+            <div className='mountainousbackground right'>
+                <div className='gethelp'>
+                  <h1>Empower Yourself with Training</h1>
+                  <ul>
+                    <li>Finding Funding</li>
+                    <li>Writing a Winning Grant</li>
+                    <li>Managing Grant Funds</li>
+                  </ul>
+
+                  <div className='help-button'>
+                    <a href =''>More</a>
+                  </div>
+                
+                </div>
+             </div>
+
+          </div>
+
+          <div className='section-6'>
+                <h1>How We Help</h1>
+          </div>
+
+          <div className='section-7'>
+              <div className='foursection'>
+                <div className='sec-1'>
+                  <h3>Assessment</h3>
+                  <ul>
+                    <li>Community Health Needs Assessments</li>
+                    <li>Feasibility Studies</li>
+                    <li>Environmental Scanning</li>
+                  </ul>
+                </div>
+                <div className='sec-2'>
+                  <h3>People</h3>
+                    <ul>
+                    <li>Leadership Retreats</li>
+                    <li>MBTI (what's MBTI?)</li>
+                    <li>Emotional Intelligence</li>
+                    <li>Adult Learning</li>
+                    <li>Trauma-informed Leadership</li>
+                    </ul>
+
+                </div>
+                <div className='sec-3'>
+                  <h3>Process</h3>
+                  <ul>
+                    <li>Technical Assistance</li>
+                    <li>Project Planning</li>
+                    <li>Resource Development and Connection</li>
+                    <li>Funding Stragegies</li>
+                  </ul>
+                </div>
+                <div className='sec-4'>
+                  <h3>Evolution</h3>
+                  <ul>
+                    <li>Strategic Planning</li>
+                    <li>Business Planning</li>
+                    <li>Leadership Development</li>
+                  </ul>
+                </div>
+              </div>
+          </div>
+
+          <div className='section-8'>
+            <h2>We cultivate space for capacity, restore ecosystems, and shift paradigms with partners in meaningful change.</h2>
+            <div className='lightbulb-pic'></div>
+          </div>
+
+          <div className='section-9'>
+            <div className='quotes-list'></div>
+          </div>
+
+          <div className='section-10'>
+            <div className='logobox'></div>
+            <div className = 'infobox'>
+              <ul>
+                <li><a href = ''>Email: emailhere</a></li>
+                <li><a href = ''>Book with this person</a></li>
+                <li><a href = ''>Book with that person</a></li>
+              </ul>
+              
+              
+              
+
+              <p>We are always pleased to hear from organizations interested in maximizing their capacity.  Contact us today.</p>
+            </div>
+          </div>
+
+          <div className='bottom'>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='footer'> 2025</div>
+
+  
+    </div>
+  );
+}
+
+export default App;
