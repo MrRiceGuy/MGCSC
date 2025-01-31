d6e08dd61333f40831288e2971f8d098c31a3f71
MrRiceGuy <carsonkeene@gmail.com>
2025-01-31 05:21:41 -0500

    Added styling, links, custom picture.

--- src/App.js
+++ src/App.js
@@ -26,10 +26,17 @@ function App() {
             </div>
           </div>
 
-          <div className='section-1'>
+          <div className='section-1 video-div'>
+                  <video autoPlay loop muted playsInline>
+                <source
+                  src="/images/yellowColorado.mp4"
+                  type="video/mp4"
+                />
+              </video>
+
             <h1>Building Resilience for Health Equity</h1>
 
-            <div className='meetwithus'><a href =''>Meet with Us</a></div>
+            <div className='meetwithus'><a href ='https://calendly.com/mgalaviz21'>Meet with Us</a></div>
             <div className='meetwithus'><a href =''>Meet Our Team</a></div>
           </div>
 
@@ -38,7 +45,7 @@ function App() {
               <li><h2>Assessment</h2>
               <p>Know where you're going and why, with full community buy-in, and continually improve.</p></li>
               
-              <li><h2>People</h2><p>Build COhesive and energized teams that understand and tust each other.</p></li>
+              <li><h2>People</h2><p>Build Cohesive and energized teams that understand and tust each other.</p></li>
               
               <li><h2>Processes</h2><p>Clearly map your mission's success, and activate support to overcome barriers.</p></li>
               
@@ -47,10 +54,10 @@ function App() {
           </div>
 
           <div className='section-3'>
-            <h2>Solving big roblems takes time.</h2>
+            <h1>Solving big problems takes time.</h1>
             <h2 className='yellow'>Are you ready for that journey?</h2>
             <ul>
-              <li><h3>Are you uncertain about the root causes of your community's challenges?</h3></li>
+              <li><h3>Are you unsure about the root causes of your community's challenges?</h3></li>
               <li><h3>Is it hard to see how all the pieces fit together?</h3></li>
               <li><h3>Are the next steps unclear or confusing?</h3></li>
               <li><h3>Is your team overwhelmed, turning over, or struggling to hire?</h3></li>
@@ -68,15 +75,26 @@ function App() {
           </div>    
 
           <div className='section-5'>
-            <div className='mountainousbackground left'>
-              <h1>Built To Last</h1>
 
-              <h3>We help changemakers and mission-driven organizations build resiliency at the organizational level to promote health equity within their own communities. You are creating health equity; we support you in establishing the practices and systems to keep your mission thriving and sustainable.</h3>
+            <div className='mountainousbackground-left'>
+
             </div>
 
-            <div className='mountainousbackground right'>
+            <div className='mountainousbackground-right'>
+               
+             </div>
+
+             <div className = 'mountainWords'>
+             <h1>Built To Last</h1>
+             <h3>We help changemakers build resiliency.</h3>
+             </div>
+
+          </div>
+
+          <div className='section-6'>
+                <h1>How We Help</h1>
                 <div className='gethelp'>
-                  <h1>Empower Yourself with Training</h1>
+                  <h1>Empower You with Training</h1>
                   <ul>
                     <li>Finding Funding</li>
                     <li>Writing a Winning Grant</li>
@@ -88,17 +106,11 @@ function App() {
                   </div>
                 
                 </div>
-             </div>
-
-          </div>
-
-          <div className='section-6'>
-                <h1>How We Help</h1>
           </div>
 
           <div className='section-7'>
-              <div className='foursection'>
-                <div className='sec-1'>
+         
+                <div className='sec'>
                   <h3>Assessment</h3>
                   <ul>
                     <li>Community Health Needs Assessments</li>
@@ -106,18 +118,18 @@ function App() {
                     <li>Environmental Scanning</li>
                   </ul>
                 </div>
-                <div className='sec-2'>
+                <div className='sec'>
                   <h3>People</h3>
                     <ul>
                     <li>Leadership Retreats</li>
-                    <li>MBTI (what's MBTI?)</li>
+                    <li>MBTI</li>
                     <li>Emotional Intelligence</li>
                     <li>Adult Learning</li>
                     <li>Trauma-informed Leadership</li>
                     </ul>
 
                 </div>
-                <div className='sec-3'>
+                <div className='sec'>
                   <h3>Process</h3>
                   <ul>
                     <li>Technical Assistance</li>
@@ -126,7 +138,7 @@ function App() {
                     <li>Funding Stragegies</li>
                   </ul>
                 </div>
-                <div className='sec-4'>
+                <div className='sec'>
                   <h3>Evolution</h3>
                   <ul>
                     <li>Strategic Planning</li>
@@ -134,25 +146,20 @@ function App() {
                     <li>Leadership Development</li>
                   </ul>
                 </div>
-              </div>
+            
           </div>
 
           <div className='section-8'>
             <h2>We cultivate space for capacity, restore ecosystems, and shift paradigms with partners in meaningful change.</h2>
             <div className='lightbulb-pic'></div>
           </div>
-
-          <div className='section-9'>
-            <div className='quotes-list'></div>
-          </div>
-
           <div className='section-10'>
             <div className='logobox'></div>
             <div className = 'infobox'>
               <ul>
-                <li><a href = ''>Email: emailhere</a></li>
-                <li><a href = ''>Book with this person</a></li>
-                <li><a href = ''>Book with that person</a></li>
+                <li>Email us at info@verticalstrategies.net</li>
+                <li><a href = 'https://calendly.com/mbosworth-2'>Book with Melissa</a></li>
+                <li><a href = 'https://calendly.com/mgalaviz21'>Book with Megan</a></li>
               </ul>
               
               
